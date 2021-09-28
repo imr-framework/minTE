442fd2af78487ab8ca96f302b52cd0c493bd2d02
Gehua Tong <31249056+tonggehua@users.noreply.github.com>
2021-09-28 14:34:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open(str(here / 'requirements.txt'), 'r') as f:
     install_reqs = install_reqs.split("\n")
 
 setuptools.setup(
-    name='mr-minTE',
-    version='0.0.1',
+    name='minTE',
+    version='0.0.2',
     author='imr-framework',
     author_email='imr.framework2018@gmail.com',
     description='Minimum TE MR sequences using PyPulseq',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/imr-framework/mr-minTE',
+    url='https://github.com/imr-framework/minTE',
     packages=setuptools.find_packages(),
     install_requires=install_reqs,
     license='License :: OSI Approved :: GNU Affero General Public License v3',
